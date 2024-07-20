39adfbf2d5cec89eaa0630ed31c6eb4fbf81d5c4
giwankim <giwan.kim@gmail.com>
2024-07-20 12:53:29 +0900

    3단계 - 경로 조회 기능 (#826)
    * test(path): 경로 조회 기능 인수 테스트를 추가한다.
    
    * feat(path): 경로를 나타내는 도메인 모델을 추가한다.
    
    * refactor(path): 경로를 나타내는 도메인 모델을 수정했다.
    
    * feat(path): 자하철을 그래프로 형상화하는 도메인 모델을 추가한다.
    
    * feat(path): 그래프가 같은지 비교하는 메서드를 구현했다.
    
    `AbstractGraph` 클래스 구현 (https://github.com/jgrapht/jgrapht/blob/master/jgrapht-core/src/main/java/org/jgrapht/graph/AbstractGraph.java) 을 참조헸다.
    
    * feat(path): 경로 조회 예외 상황 처리 기능을 구현했다.
    
    * feat(path): 노선을 추가하는 기능을 구현했다.
    
    * feat(path): 지하철 그래프를 불러오는 서비스 클래스를 구현했다.
    
    * feat(path): 경로 조회 서비스를 구현했다.
    
    * feat(path): 경로 조회 controller를 구현했다.
    
    * fix(path): 인수 테스트에 빠진 테스트 픽스처를 추가했다.
    
    * refactor(path): `LineSection` 을 기반으로 한 도메인 그래프 간선 모델을 추가한다.
    
    * refactor(path): 사용하지 않는 `SubwayGraph#toString` 메서드를 제거했다.
    
    * refactor(path): 접근 제어자를 적절하게 수정했다.
    
    * refactor(path): double 을 비교하기 위한 작은 상수인 epsilon 을 추출했다.

--- src/main/java/nextstep/subway/station/domain/Station.java
+++ src/main/java/nextstep/subway/station/domain/Station.java
@@ -1,10 +1,12 @@
 package nextstep.subway.station.domain;
 
+import java.util.Objects;
 import javax.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.proxy.HibernateProxy;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -29,5 +31,35 @@ public class Station {
 
   public boolean isSame(Station other) {
     return this.id.equals(other.id);
+  }
+
+  @Override
+  public final boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null) {
+      return false;
+    }
+    Class<?> oEffectiveClass =
+        o instanceof HibernateProxy
+            ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass()
+            : o.getClass();
+    Class<?> thisEffectiveClass =
+        this instanceof HibernateProxy
+            ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass()
+            : this.getClass();
+    if (thisEffectiveClass != oEffectiveClass) {
+      return false;
+    }
+    Station station = (Station) o;
+    return getId() != null && Objects.equals(getId(), station.getId());
+  }
+
+  @Override
+  public final int hashCode() {
+    return this instanceof HibernateProxy
+        ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode()
+        : getClass().hashCode();
   }
 }
