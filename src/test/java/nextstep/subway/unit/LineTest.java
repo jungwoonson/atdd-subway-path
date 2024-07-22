7f229711456edc012a5d3002ab26ad1c4422b4fd
jungwoonson <jungwoon727@gmail.com>
2024-07-22 17:46:29 +0900

    refactor: 구간 추가 유닛 테스트 리팩터링

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -2,30 +2,45 @@ package nextstep.subway.unit;
 
 import nextstep.subway.line.Line;
 import nextstep.subway.station.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
-    @Test
-    @DisplayName("구간을 추가한다.")
-    void addSection() {
-        Station 강남역 = Station.of(1L, "강남역");
-        Station 성수역 = Station.of(2L, "성수역");
-        Station 건대입구역 = Station.of(3L, "건대입구역");
 
-        Line line = Line.builder()
+    private Station 강남역;
+    private Station 성수역;
+    private Station 건대입구역;
+    private Line 신분당선;
+
+    @BeforeEach
+    void setUp() {
+        강남역 = Station.of(1L, "강남역");
+        성수역 = Station.of(2L, "성수역");
+        건대입구역 = Station.of(3L, "건대입구역");
+        신분당선 = 신분당선(강남역, 성수역);
+    }
+
+    private Line 신분당선(Station upStation, Station downStation) {
+        return Line.builder()
                 .id(1L)
                 .name("2호선")
                 .color("bg-red-600")
-                .upStation(강남역)
-                .downStation(성수역)
+                .upStation(upStation)
+                .downStation(downStation)
                 .build();
+    }
 
-        line.registerSection(성수역, 건대입구역, 10);
+    @Test
+    @DisplayName("구간을 추가한다.")
+    void addSection() {
+        // when
+        신분당선.registerSection(성수역, 건대입구역, 10);
 
-        assertThat(line.getStationIds()).containsExactly(강남역.getId(), 성수역.getId(), 건대입구역.getId());
+        // then
+        assertThat(신분당선.getStationIds()).containsExactly(강남역.getId(), 성수역.getId(), 건대입구역.getId());
     }
 
     @Test
