7b97a868e94cc9384710400da6698d5cf47ff644
jungwoonson <jungwoon727@gmail.com>
2024-07-24 15:34:09 +0900

    refactor: EOF 추가 및 사용하지 않는 코드 제거

--- src/main/java/nextstep/subway/station/StationRepository.java
+++ src/main/java/nextstep/subway/station/StationRepository.java
@@ -3,5 +3,4 @@ package nextstep.subway.station;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StationRepository extends JpaRepository<Station, Long> {
-    String findNameById(Long stationId);
 }
